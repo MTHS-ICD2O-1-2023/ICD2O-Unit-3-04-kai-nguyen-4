9cb14299e6b8dd94bfd653cc66fd7c3f5426540a
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-04-10 18:41:20 +0000

    ..

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@
 // This file contains the JS functions for index.html
 "use strict"
 
-function findCelciusClicked() {
+function findCelsiusClicked() {
   // Use parseFloat so the user can input a decimal
   const tempInput = parseFloat(document.getElementById("temp-entered").value)
   // Create a variable named "celsius" which calculates the conversion formula and add decimals to the end so the system will use the fraction as a float instead of an integer
-  const celcius = (tempInput - 32) * 5.0/9.0
+  const celsius = (tempInput - 32) * 5.0/9.0
   // Ouput the answer to the user in the class "answer"
-  document.getElementById("answer").innerHTML = "If the temperature in fahrenheit is " + tempInput + "<sup>o</sup>F" + ", then the temperature in celcius is " + celcius.toFixed(3) + "<sup>o</sup>C"
+  document.getElementById("answer").innerHTML = "If the temperature in fahrenheit is " + tempInput + "<sup>o</sup>F" + ", then the temperature in celsius is " + celsius.toFixed(3) + "<sup>o</sup>C"
 }
